2881548193bd2269e5464fd8789631af5c0bea75
Dan Wilder <12650041+WilderDan@users.noreply.github.com>
2024-10-15 09:37:13 -0500

    Add game state

---
+++ scripts/game.gd
@@ -0,0 +1,16 @@
+extends Node2D
+
+enum State {Title, Playing}
+
+@onready var state = State.Title
+
+func _input(event):
+	if event.is_action_pressed("tap"):
+		match state:
+			State.Title:
+				start_game()
+				
+func start_game():
+	state = State.Playing
+	$TitleMessage.hide()
+	$Player.is_active = true
